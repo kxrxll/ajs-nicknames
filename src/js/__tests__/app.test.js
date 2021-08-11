9d97ebf04dc69330e397cf3503890b5cc4d32690
kxrxll <kovalyovkirill@gmail.com>
2021-08-11 13:06:47 +0300

    solved

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,26 @@
-import sum from '../basic';
+import Validator from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('case 1', () => {
+  const result = Validator.validateUsername('Emma_Wotson');
+  expect(result).toBe(true);
+});
 
-  expect(result).toBe(6);
+test('case 2', () => {
+  const result = Validator.validateUsername('R@se');
+  expect(result).toBe(false);
+});
+
+test('case 3', () => {
+  const result = Validator.validateUsername('10Eric');
+  expect(result).toBe(false);
+});
+
+test('case 4', () => {
+  const result = Validator.validateUsername('Pete-');
+  expect(result).toBe(false);
+});
+
+test('case 4', () => {
+  const result = Validator.validateUsername('Pete-Fu1234g');
+  expect(result).toBe(false);
 });
